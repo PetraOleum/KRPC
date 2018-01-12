e8cc4d500e693d29208080a98d3319fc69842778
Petra Lamborn <oleumpetra@gmail.com>
2018-01-12 19:25:40 +1300

    Plane still stalls

--- plane.py
+++ plane.py
@@ -4,12 +4,19 @@ import time
 import krpc
 
 # The IP of the KRPC server (if not same computer)
-remote = '192.169.1.10'
+remote = '192.168.1.10'
+# Make connection
 conn = krpc.connect(name='Plane autopilot', address=remote)
+
+# Important objects
 vessel = conn.space_center.active_vessel
 body = vessel.orbit.body
 r_frame = body.reference_frame
+surf_vel = conn.get_call(getattr, vessel.flight(r_frame), 'horizontal_speed')
+srf_altitude = conn.get_call(getattr, vessel.flight(), 'surface_altitude')
+vert_vel = conn.get_call(getattr, vessel.flight(r_frame), 'vertical_speed')
 print(vessel.name)
+
 vessel.control.brakes = True
 
 vessel.auto_pilot.target_pitch_and_heading(0, 90)
@@ -22,7 +29,6 @@ vessel.control.activate_next_stage()
 time.sleep(3)
 vessel.control.brakes = False
 
-surf_vel = conn.get_call(getattr, vessel.flight(r_frame), 'horizontal_speed')
 
 expr = conn.krpc.Expression.greater_than(
     conn.krpc.Expression.call(surf_vel),
@@ -34,7 +40,6 @@ with event.condition:
 print('Tilt up')
 vessel.auto_pilot.target_pitch_and_heading(20, 90)
 
-srf_altitude = conn.get_call(getattr, vessel.flight(), 'surface_altitude')
 
 expr = conn.krpc.Expression.greater_than(
     conn.krpc.Expression.call(srf_altitude),
@@ -58,6 +63,7 @@ vessel.auto_pilot.target_pitch_and_heading(5, 90)
 vessel.control.throttle = 0.20
 time.sleep(35)
 
+print('turn')
 vessel.control.roll = 0.03
 vessel.auto_pilot.target_pitch_and_heading(5, 120)
 time.sleep(8)
@@ -66,20 +72,21 @@ time.sleep(8)
 vessel.auto_pilot.target_pitch_and_heading(5, 210)
 time.sleep(8)
 vessel.auto_pilot.target_pitch_and_heading(2, 270)
-time.sleep(8)
+
 vessel.control.roll = 0.00
-vessel.control.throttle = 0
-vessel.auto_pilot.target_pitch_and_heading(-15, 270)
+vessel.control.throttle = 0.025
 
-expr = conn.krpc.Expression.less_than(
-    conn.krpc.Expression.call(srf_altitude),
-    conn.krpc.Expression.constant_double(300))
-event = conn.krpc.add_event(expr)
-with event.condition:
-    event.wait()
+while vessel.flight(r_frame).horizontal_speed > 90:
+    if vessel.flight(r_frame).surface_altitude > 100:
+        print('Nose down')
+        vessel.auto_pilot.target_pitch_and_heading(-5, 270)
+    elif vessel.flight(r_frame).surface_altitude < 75:
+        print('Nose up')
+        vessel.auto_pilot.target_pitch_and_heading(7, 270)
+    time.sleep(1)
 
-vessel.auto_pilot.target_pitch_and_heading(2, 270)
 vessel.control.gear = True
+vessel.auto_pilot.target_pitch_and_heading(3, 270)
 
 expr = conn.krpc.Expression.less_than(
     conn.krpc.Expression.call(srf_altitude),
@@ -88,23 +95,8 @@ event = conn.krpc.add_event(expr)
 with event.condition:
     event.wait()
 
-vessel.auto_pilot.target_pitch_and_heading(14, 270)
-
-time.sleep(5)
-
-vert_vel = conn.get_call(getattr, vessel.flight(r_frame),
-                         'vertical_speed')
-
-expr = conn.krpc.Expression.less_than(
-    conn.krpc.Expression.call(vert_vel),
-    conn.krpc.Expression.constant_double(0))
-event = conn.krpc.add_event(expr)
-with event.condition:
-    event.wait()
-
-vessel.auto_pilot.target_pitch_and_heading(5, 270)
-
 vessel.control.brakes = True
 
+vessel.control.throttle = 0
 
 conn.close()
